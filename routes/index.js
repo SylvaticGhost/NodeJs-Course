2ab553e5fcb60a95ca4d481e7714764e6ab449bb
SylvaticGhost <k.tkachov05@gmail.com>
2025-03-17 15:56:11 +0200

    dddd

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  if (req.user) {
+    res.sendFile(path.join(__dirname, '../views/index.html'));
+  } else {
+    res.sendFile(path.join(__dirname, '../views/auth.html'));
+  }
 });
 
 module.exports = router;
